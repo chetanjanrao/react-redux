08a4a2afdbd813044ed76651d331e25f37ea71ee
Chetan Janrao <jrchetan1997@gmail.com>
2025-05-31 14:58:22 +0530

    React Redux till video 7 App working

---
+++ Redux-Demo.js
@@ -0,0 +1,34 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const initialState = {
+      numOfCakesInShelf : 10
+}
+
+const BUY_CAKES = "BUY_CAKES";
+function buyCakes(){
+      return {
+            type : "BUY_CAKES",
+            info : "React Redux first action"
+      }
+}
+
+const reducer = (state = initialState, action) =>{
+//      console.log("state",state)
+      switch(action.type){
+            case "BUY_CAKES": return {
+                  ...state,
+                  numOfCakesInShelf : state.numOfCakesInShelf - 1
+            }
+             default : return state;
+      }
+     
+}
+
+const store = createStore(reducer);
+console.log("initail State : ",store.getState())
+const unsubscribe = store.subscribe(()=>{console.log("updated State : ", store.getState())})
+store.dispatch(buyCakes())
+store.dispatch(buyCakes())
+store.dispatch(buyCakes())
+unsubscribe();
