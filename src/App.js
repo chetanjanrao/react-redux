1a17c4d667e3597f14bf45b44dd88b14ca593bd6
Chetan Janrao <jrchetan1997@gmail.com>
2025-06-01 20:20:33 +0530

    Redux with useSector and useDispatch hooks

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { Provider } from "react-redux"
 import store from './Redux/Cakes/Store';
 import "./App.css"
 import CakeContainer from './Components/CakeContainer';
+import HooksCakeContainer from "./Components/HooksCakeContainer";
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <div className="container">
           <h2 style={{textDecoration:"underline"}}>Cake app</h2>
-          <CakeContainer />
+          {/* <CakeContainer /> */}
+        </div>
+        <div className="container">
+          <HooksCakeContainer/>
         </div>
       </div>
     </Provider>
